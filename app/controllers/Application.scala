c7ae2495952294b5563aabef1a24d85ef01dd0f6
Sergey80 <ses.box@gmail.com>
2015-07-08 21:46:26 -0400

    added: Iteratees (instead of WS)
    + added UI chat demo
    added "feed", "postMessage" methods to the controller
    
    on "postMessage" send message-info to the our top-level HistoryActor

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,44 +1,50 @@
 package controllers
 
-import actors.msg.{ReadNextMsg, WriteMsg, ReadAtMsg}
+import play.api.libs.EventSource
+import actors.msg.{ReadNextMsg, WriteMsg}
 import actors.{HistoryActor}
 import akka.actor.{ActorSystem, Props}
+import play.api.libs.iteratee.{Enumerator, Concurrent}
+import play.api.libs.json.{Json,JsValue}
 import play.api.mvc._
-import scala.concurrent.duration.Duration
 
-import play.api.Play.current // brings the current running Application into context
+import play.api.Play.current   // brings the current running Application into context
 
 object Application extends Controller {
 
+  val system = ActorSystem("historySystem")
 
-// TODO: WS
-//  def socket = WebSocket.acceptWithActor[JsValue, JsValue] { request => out =>
-//    MyWebSocketActor.props(out)
-//  }
+  //  create top-level actors
+  val historyActor = system.actorOf(
+    Props(classOf[HistoryActor], "history-file.log"),
+    name = "history-actor"
+  )
 
-  // TODO: will be WS, just testing for now
-  def index = Action {
+  val (out, channel) = Concurrent.broadcast[JsValue]
 
-    val system = ActorSystem("historySystem")
+  def feed = Action { req =>
+    println("Someone connected")
+    Ok.chunked(welcome >>> out &> EventSource()).as("text/event-stream")
+  }
 
-    //  create top-level actors
-    val historyActor = system.actorOf(
-                          Props(classOf[HistoryActor], "history-file.log"),
-                          name = "history-actor"
-                        )
+  def postMessage = Action(parse.json) { req => {
 
-    // just to to feel that it works / send msg and forwards it properly (see the logs)
-    // then we should provide with proper testings
+    historyActor ! WriteMsg( req.body.toString() )      // send a message-info to the HistoryActor /lib, to store
 
-    historyActor ! WriteMsg("line 1")
-    historyActor ! WriteMsg("line 2")
-    historyActor ! WriteMsg("line 3")
+//    historyActor ! ReadNextMsg
 
-    historyActor ! ReadNextMsg
-    historyActor ! ReadNextMsg
-    historyActor ! ReadNextMsg
+    channel.push(req.body)
 
-    Ok(views.html.index("History Demo"))
+    Ok
+  }}
+
+  def welcome = Enumerator.apply[JsValue](Json.obj(
+    "user" -> "Bot",
+    "message" -> "Welcome! Enter a message and hit Enter"
+  ))
+
+  def index = Action { implicit req =>
+    Ok(views.html.index(routes.Application.feed(), routes.Application.postMessage()))
   }
 
 }
